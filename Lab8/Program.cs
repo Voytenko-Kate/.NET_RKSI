e6130530e34f8272b54a32fa14bd18531a8e4a33
h10ne <44882885+h10ne@users.noreply.github.com>
2019-04-05 13:52:15 +0300

    8 done

---
+++ Lab8/Program.cs
@@ -0,0 +1,44 @@
+﻿//
+//
+// Часть 1. Реализовать пошаговую форму заполнения данных студента.
+// Реализовать три формы для заполнения данных по частям:
+//   Личные данные,
+//   Контактные данные,
+//   Данные об обучении.
+// Данные должны проверяться на корректность.
+//
+// Часть 2.Реализовать главную форму с возможностью сохранения и загрузки данных
+//
+
+using System;
+using System.Xml.Serialization;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+
+namespace Lab8
+{
+    static class Program
+    {
+
+        private static StudentData student = new StudentData();
+
+        static void OnShutdown(object sender, EventArgs args)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(StudentData));
+            var stream = new StreamWriter("test.xml");
+            serializer.Serialize(stream, student);
+            stream.Close();
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);         
+            
+            Application.Run(new MainForm());
+        }
+
+    }
+}
